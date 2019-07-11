828e2a8ac15f10d62631605484a415fed6a6a450
cairui <18811610600@163.com>
2019-07-11 22:02:18 +0800

    完成首页分页菜单，已经把相关视图迁移，请使用fragment文件夹中对应的视图响应，数据库待整合！

---
+++ app/src/main/java/com/example/notebook_test/Fregment/today.java
@@ -0,0 +1,141 @@
+package com.example.notebook_test.Fregment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.notebook_test.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link today.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link today#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class today extends Fragment{
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    public today() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment today.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static today newInstance(String param1, String param2) {
+        today fragment = new today();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =inflater.inflate(R.layout.fragment_today, container, false);
+        Bundle args = getArguments();
+        //测试可用于从其他act中传值
+//        if (args != null) {
+//            context = args.getString("context");
+//        }
+        //listview的事件
+
+        final List<String> items = new ArrayList<String>(); //设置要显示的数据，这里因为是例子，所以固定写死
+        items.add("item1");
+        items.add("item2");
+        items.add("item3");
+        items.add("item4");
+        items.add("item5");
+        items.add("item6");
+        ListView listView =  (ListView)view.findViewById(R.id.listView1); // 从布局中获取listview，也可以动态创建
+        listView.setAdapter(new ArrayAdapter<String>(getActivity(),android.R.layout.simple_expandable_list_item_1, items));//关联Adapter//将ListView加到适配器里
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() { //设置点击ListView中的条目的响应对象
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, //响应方法，其中view是一个TextView对象，position是选择条目的序号
+                                    int position, long id) {
+                Toast.makeText(getActivity(), items.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+        return view;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
--- app/src/main/java/com/example/notebook_test/MainActivity.java
+++ app/src/main/java/com/example/notebook_test/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.notebook_test;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import com.example.notebook_test.Fregment.*;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
@@ -23,9 +30,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, add.OnFragmentInteractionListener,today.OnFragmentInteractionListener,calender.OnFragmentInteractionListener {
 
     private CalendarView cv;//定义日历控件按钮
+    private calender f1;
+    private today f2;
+    private add f3;
+    private android.support.v4.app.FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,15 +45,7 @@ public class MainActivity extends AppCompatActivity
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-//        FloatingActionButton fab = findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);//侧边栏布局 drawer
         NavigationView navigationView = findViewById(R.id.nav_view);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -50,42 +53,42 @@ public class MainActivity extends AppCompatActivity
         toggle.syncState();
         navigationView.setNavigationItemSelectedListener(this);
 
-        //listview的事件
-
-        final List<String> items = new ArrayList<String>(); //设置要显示的数据，这里因为是例子，所以固定写死
-        items.add("item1");
-        items.add("item2");
-        items.add("item3");
-        items.add("item4");
-        items.add("item5");
-        items.add("item6");
-        ListView listView =  findViewById(R.id.listView1); // 从布局中获取listview，也可以动态创建
-        listView.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_expandable_list_item_1, items));//关联Adapter
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() { //设置点击ListView中的条目的响应对象
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, //响应方法，其中view是一个TextView对象，position是选择条目的序号
-                                    int position, long id) {
-                Toast.makeText(getApplicationContext(), items.get(position), Toast.LENGTH_SHORT).show();
-            }
-        });
-
-
-        //日历的响应事件，这里先把时间提取出来
-        cv = findViewById(R.id.calendarview);
-        cv.setOnDateChangeListener(new CalendarView.OnDateChangeListener(){
-
-            @Override
-            public void onSelectedDayChange(CalendarView view, int year,
-                                            int month, int dayOfMonth) {
-                // TODO Auto-generated method stub
-                //使用Toast显示用户选择的日期
-                Toast.makeText(getApplicationContext(),
-                        "你生日是"+year+"年"+month+"月"+dayOfMonth+"日"
-                        , Toast.LENGTH_SHORT).show();
-
-            }
-        });
-
+//        //listview的事件
+//
+//        final List<String> items = new ArrayList<String>(); //设置要显示的数据，这里因为是例子，所以固定写死
+//        items.add("item1");
+//        items.add("item2");
+//        items.add("item3");
+//        items.add("item4");
+//        items.add("item5");
+//        items.add("item6");
+//        ListView listView =  findViewById(R.id.listView1); // 从布局中获取listview，也可以动态创建
+//        listView.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_expandable_list_item_1, items));//关联Adapter
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() { //设置点击ListView中的条目的响应对象
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, //响应方法，其中view是一个TextView对象，position是选择条目的序号
+//                                    int position, long id) {
+//                Toast.makeText(getApplicationContext(), items.get(position), Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//
+//
+//        //日历的响应事件，这里先把时间提取出来
+//        cv = findViewById(R.id.calendarview);
+//        cv.setOnDateChangeListener(new CalendarView.OnDateChangeListener(){
+//
+//            @Override
+//            public void onSelectedDayChange(CalendarView view, int year,
+//                                            int month, int dayOfMonth) {
+//                // TODO Auto-generated method stub
+//                //使用Toast显示用户选择的日期
+//                Toast.makeText(getApplicationContext(),
+//                        "你生日是"+year+"年"+month+"月"+dayOfMonth+"日"
+//                        , Toast.LENGTH_SHORT).show();
+//
+//            }
+//        });
+        bindView();
     }
 
     @Override
@@ -148,5 +151,71 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            fragmentManager = getSupportFragmentManager();
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+            hideAllFragment(transaction);
+            hideAllFragment(transaction);
+            switch (item.getItemId()) {
+                case R.id.navigation_calender:
+                    if (f1 == null) {
+                        f1 = calender.newInstance("hello","world");
+                        transaction.add(R.id.fragment_container, f1);
+                    } else {
+                        transaction.show(f1);
+                    }
+                    break;
+                case R.id.navigation_today:
+                    if (f2 == null) {
+                        f2 = today.newInstance("hello","world");
+                        transaction.add(R.id.fragment_container, f2);
+                    } else {
+                        transaction.show(f2);
+                    }
+                    break;
+                case R.id.navigation_add:
+                    if (f3 == null) {
+                        f3 = add.newInstance("hello","world");
+                        transaction.add(R.id.fragment_container, f3);
+                    } else {
+                        transaction.show(f3);
+                    }
+                    break;
+            }
+            transaction.commit();
+            return true;
+        }
+    };
+
+    //UI组件初始化与事件绑定
+    private void bindView() {
+        BottomNavigationView navView = findViewById(R.id.bnv_view);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+
+
+    //隐藏所有Fragment
+    public void hideAllFragment(FragmentTransaction transaction) {
+        if (f1 != null) {
+            transaction.hide(f1);
+        }
+        if (f2 != null) {
+            transaction.hide(f2);
+        }
+        if (f3 != null) {
+            transaction.hide(f3);
+        }
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
